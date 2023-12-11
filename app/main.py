28526286d1eb406821ec57b2271b5ecf83572b8e
camigrandy <camgany@gmail.com>
2023-12-11 17:00:15 -0400

    fix: main

--- app/main.py
+++ app/main.py
@@ -1,96 +1,83 @@
 import argparse
-import sys
-import time
-
 import cv2
 import mediapipe as mp
+import time
 
 from app.detector import MediapipeStreamObjectDetector
-from app.utils import visualize
 
-
-def run(
-    model: str, camera_id: int, width: int, height: int, has_gui: bool, threshold: float
-) -> None:
-    counter = 0
-    fps = 0
-    start_time = time.time()
-    # Visualization parameters
-    row_size = 20  # pixels
-    left_margin = 24  # pixels
-    text_color = (0, 0, 255)  # red
-    font_size = 1
-    font_thickness = 1
-    fps_avg_frame_count = 10
-    print("Starting video stream...")
+def show_camera_and_detection(model, camera_id, width, height, has_gui, threshold):
     cap = cv2.VideoCapture(camera_id)
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+
+    if not cap.isOpened():
+        print("Error al abrir la cámara.")
+        return
 
     # instantiate detector
     print("Calling object detector...")
     detector = MediapipeStreamObjectDetector(model, threshold)
 
-    # main loop
-    while cap.isOpened():
-        success, image = cap.read()
-        if not success:
-            sys.exit("Could not read from webcam")
+    if has_gui:
+        cv2.startWindowThread()
+        cv2.namedWindow("Cámara", cv2.WINDOW_NORMAL)
+        cv2.imshow("Cámara", cv2.cvtColor(cv2.imread("black_image.jpg"), cv2.COLOR_BGR2RGB))
+        time.sleep(0.1)  # Agregar una pequeña pausa
 
-        # counter for detections
-        counter += 1
-        # preprocess image
+    num_persons_prev = 0  # Número de personas en el fotograma anterior
+
+    while True:
+        ret, image = cap.read()
+
+        if not ret:
+            print("Error al capturar el frame.")
+            break
+
         image = cv2.flip(image, 1)
         rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_image)
-        detector.detector.detect_async(mp_image, counter)
+        # Proporcionar la marca de tiempo en milisegundos
+        timestamp_ms = int(time.time() * 1000)
+        detector.detector.detect_async(mp_image, timestamp_ms)
+
         current_frame = mp_image.numpy_view()
         current_frame = cv2.cvtColor(current_frame, cv2.COLOR_RGB2BGR)
 
-        if counter % fps_avg_frame_count == 0:
-            end_time = time.time()
-            fps = fps_avg_frame_count / (end_time - start_time)
-            start_time = time.time()
-
-        fps_text = f"FPS: {fps:.1f}"
         if has_gui:
-            text_location = (left_margin, row_size)
-            cv2.putText(
-                current_frame,
-                fps_text,
-                text_location,
-                cv2.FONT_HERSHEY_PLAIN,
-                font_size,
-                text_color,
-                font_thickness,
-            )
+            cv2.imshow("Cámara", current_frame)
 
         if detector.result_list:
-            # print(detector.result_list)
-            if has_gui:
-                vis_image = visualize(current_frame, detector.result_list[0])
-                cv2.imshow("object_detector", vis_image)
-            else:
-                detections_dict = {
-                    label: score
-                    for label, score in zip(
-                        detector.result_list[0].labels,
-                        detector.result_list[0].confidences,
-                    )
-                }
-                print(f"{fps_text} \t Detections: {detections_dict}")
+            num_persons_current = 0
+            for i, detection in enumerate(detector.result_list):
+                # Filtrar detecciones para mantener solo las personas
+                person_detections = [
+                    (label, score)
+                    for label, score in zip(detection.labels, detection.confidences)
+                    if "person" in label.lower()
+                ]
+                num_persons_current += len(person_detections)
+
+                if person_detections:
+                    detections_dict = {
+                        f"Person_{i+1}_{label}": score
+                        for label, score in person_detections
+                    }
+                   # print(f"Detections for Person {i+1}: {detections_dict}")
+
+            if num_persons_current != num_persons_prev:
+                print(f"Se encontraron {num_persons_current} persona(s).")
+                num_persons_prev = num_persons_current
+
             detector.result_list.clear()
 
-        elif has_gui:
-            cv2.imshow("object_detector", current_frame)
-
-        if cv2.waitKey(1) == 27:
+        key = cv2.waitKey(1)
+        if has_gui and key == 27:  # 27 es el código ASCII para la tecla 'Esc'
             break
+
+    if has_gui:
+        time.sleep(0.1)  # Agregar una pequeña pausa
+        cv2.destroyAllWindows()
 
     detector.detector.close()
     cap.release()
-    cv2.destroyAllWindows()
-
 
 def main():
     parser = argparse.ArgumentParser(
@@ -135,7 +122,7 @@ def main():
     args = parser.parse_args()
     print("Starting...")
 
-    run(
+    show_camera_and_detection(
         args.model,
         int(args.cameraId),
         args.frameWidth,
@@ -143,7 +130,6 @@ def main():
         args.has_gui,
         args.threshold,
     )
-
 
 if __name__ == "__main__":
     main()
